a39222ee19aa2864465d60a3232465006c9b81c0
Book-reader <thevoid@outlook.co.nz>
2025-08-26 19:12:41 +1200

    resolve deprecation warnings & fix errors when using optional function

--- generator.py
+++ generator.py
@@ -2,11 +2,14 @@ print("module closure;")
 # print("import std::io;")
 
 print("macro @generate_closure_call_func($OrigFnType, usz $offset, ...) @private\n{")
-print("\tvar $params = $OrigFnType.params;")
-print("\t$switch $OrigFnType.params.len:")
+print("\tvar $params = {};")
+print("\t$foreach $param : $OrigFnType.paramsof:")
+print("\t\t$params = $params +++ {$param.type};")
+print("\t$endforeach")
+print("\t$switch $params.len:")
 for i in range(10 + 1):
 	print(f"\t$case {i}:")
-	print("\t\t$switch $OrigFnType.params.len - $vacount:")
+	print("\t\t$switch $params.len - $vacount:")
 	for j in range(i + 1):
 		print(f"\t\t$case {j}:")
 		print("\t\t\tvar $NewFnType = $typeof(fn $typefrom($OrigFnType.returns)(", end = '')
@@ -25,7 +28,7 @@ for i in range(10 + 1):
 		print("\t\t\t\t\tassert(temp_closure_ptr);")
 		print("\t\t\t\t\tclosure_ptr = temp_closure_ptr;")
 		print("\t\t\t\t\ttemp_closure_ptr = null;")
-		print("\t\t\t\t\t$if types::flat_type($OrigFnType.returns) == void.typeid:")
+		print("\t\t\t\t\t$if types::flat_type($OrigFnType.returns) == void.typeid ||| types::flat_type($OrigFnType.returns) == void?.typeid:")
 		print("\t\t\t\t\t\treturn;")
 		print("\t\t\t\t\t$else")
 		print("\t\t\t\t\t\treturn {};")
@@ -39,7 +42,7 @@ for i in range(10 + 1):
 		print(");")
 		
 		print("\t\t\t};")
-		print("\t\t\tf(", end = '')
+		print("\t\t\t(void)f(", end = '')
 		for k in range(j):
 			print("{}, ", end = '')
 		print(");")
