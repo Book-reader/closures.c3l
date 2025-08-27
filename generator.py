1bf4f0c1bb86e994011416f3b64c3d6db1fc3ac1
Book-reader <thevoid@outlook.co.nz>
2025-08-28 00:30:01 +1200

    IT WORKS FULLY NOW!

--- generator.py
+++ generator.py
@@ -54,26 +54,33 @@ print("\t$endswitch")
 print("}")
 
 
-print("macro @generate_closure($OrigFnType, $offset, $params, ...)@private\n{")
-print("var $fn_params = $OrigFnType.params;")
-print("$switch $fn_params.len:")
+# print("macro @generate_closure($OrigFnType, $offset, $params, ...) @private\n{")
+# print("return fn $typefrom($OrigFnType.returns)(void** wrapped, void** passed)")
+# print("}")
+
+print("module closure::closures;")
+# print("import std::io;")
+print("macro @generate_closure($OrigFnType, $offset, $params, ...) @private\n{")
+print("$switch $params.len:")
 for i in range(10 + 1):
 	print(f"$case {i}:")
-	print("\t$switch $fn_params.len - $vacount:")
+	print("\t$switch $params.len - $vacount:")
 	for j in range(i + 1):
 		print(f"\t$case {j}:")
-		print("\t\tvar $NewFnType = $typeof(fn $typefrom($OrigFnType.returns)(void**", end = '')
+		print("\t\tvar $NewFnType = $typeof(fn $typefrom($OrigFnType.returns)(", end = '')
 		for k in range(j):
-			print(f", $typefrom($fn_params[$vacount + {k}])", end = '')
+			print(f"$typefrom($params[$vacount + {k}]), ", end = '')
 		print(") => unreachable());")
 		# print("\t\tevil_hack::INIT{Closure, $NewFnType};")
-		print("\t\t$NewFnType f = fn (args", end = '')
+		print("\t\t$NewFnType f = fn (", end = '')
 		for k in range(j):
-			print(f", {chr(97 + k)}", end = '')
+			print(f"{chr(97 + k)}, ", end = '')
 		print(")\n\t\t{")
+		print("\t\t\tvoid** args = *(void***)((uptr)$$returnaddress(0) - returnaddr_offset);")
+		# print("\t\t\tio::printfn(\"Args are at address %p\", args);")
 		print("\t\t\treturn (($OrigFnType)args[0])(", end = '')
 		for k in range(i - j):
-			print(f"*($typefrom($params[{k}][0])*)args[$offset + {k}], ", end = '')
+			print(f"*($typefrom($params[{k}])*)args[{k} + $offset], ", end = '')
 		for k in range(j):
 			print(f"{chr(97 + k)}, ", end = '')
 		print(");")
